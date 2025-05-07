a97ce957ca41671005b905ea8a16d4cf7166964c
NotJonatan <60527758+NotJonatan@users.noreply.github.com>
2025-05-07 17:28:51 -0400

    gamer?

---
+++ Source/Codingcpp/Codingcpp.Build.cs
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using System.IO;
+using UnrealBuildTool;
+
+public class Codingcpp : ModuleRules
+{
+	public Codingcpp(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+        PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Public"));
+        PrivateIncludePaths.Add(Path.Combine(ModuleDirectory, "Private"));
+
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
